5296491be2d49d373ef65782b31b58677484581b
Georgii Lebeda <lebeda.georgiy@gmail.com>
2024-02-21 00:34:13 +0000

    Added players list on frontend

--- frontend/src/components/SignUpPage.js
+++ frontend/src/components/SignUpPage.js
@@ -1,79 +1,95 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setPlayers, setHasSignedUp, setHasPaid } from '../store/signupSlice';
+import { setPlayers, setGameDetails } from '../store/signupSlice';
 import axios from '../axios'; 
-import { Button, List, ListItem, ListItemText, TextField, Container, Typography } from '@mui/material';
+import { Button, TextField, Container, Typography } from '@mui/material';
+import PlayerList from './PlayerList.js'; 
 
 const SignUpPage = () => {
   const dispatch = useDispatch();
-  const { players } = useSelector((state) => state.signup);
+  const { players, gameDetails } = useSelector((state) => state.signup);
   const [playerName, setPlayerName] = useState('');
-  // Temporary for testing
-  const gameId = 'e2912220-8dce-4dc8-9881-b04c5bda1fa6';
+  const [highlightedIndex, setHighlightedIndex] = useState(null);
 
-  // useEffect(() => {
-  //   // Fetch the initial list of players
-  //   axios.get('/api/players')
-  //     .then(response => {
-  //       dispatch(setPlayers(response.data));
-  //     })
-  //     .catch(error => console.error('There was an error fetching the players:', error));
-  // }, [dispatch]);
+  // Function to fetch game details and sign-ups
+  const fetchGameDetails = () => {
+    axios.get('/signups/upcoming')
+      .then(response => {
+        const { game, signUps } = response.data;
+        dispatch(setGameDetails(game));
+        dispatch(setPlayers(signUps.map(signUp => ({
+          ...signUp,
+          name: signUp.PlayerName,
+          hasPaid: false // This can be adjusted once we have the payment status
+        }))));
+      })
+      .catch(error => console.error('There was an error fetching the game details:', error));
+  };
+
+  useEffect(() => {
+    fetchGameDetails();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHighlightedIndex(null);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [players.length]);
 
   const handleNameChange = (event) => {
     setPlayerName(event.target.value);
   };
 
-  const handleSignUp = () => {
-    // Construct the sign-up object
-       const signUpDetails = {
-        gameId: gameId,
-        playerName: playerName
-      };
-    // API call
+  const handleSignUp = (event) => {
+    event.preventDefault(); 
+    const signUpDetails = {
+      gameId: gameDetails.GameId,
+      playerName: playerName
+    };
     axios.post('/signups', signUpDetails)
       .then(response => {
-        // Handle the successful sign-up here
         console.log('Player signed up successfully', response.data);
-        // Clear the playerName or show a success message
         setPlayerName('');
+        setHighlightedIndex(players.length);
+        fetchGameDetails(); 
       })
       .catch(error => {
-        // Handle errors here
         console.error('There was an error signing up the player:', error);
-        // Show error message
       });
   };
 
   return (
     <Container maxWidth="sm">
       <Typography variant="h4" component="h1" gutterBottom>
-        Sign Up for Next Game
+        Sign Up for the Upcoming Game
       </Typography>
+      {gameDetails && (
+        <Typography variant="h6" gutterBottom>
+          {`Game Date: ${gameDetails.Date}, Location: ${gameDetails.Location}`}
+        </Typography>
+      )}
+      <form onSubmit={handleSignUp}>
       <TextField
-        label="Your Name"
-        variant="outlined"
-        fullWidth
-        value={playerName}
-        onChange={handleNameChange}
-        margin="normal"
-      />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleSignUp}
-        disabled={!playerName.trim()}
-        style={{ marginBottom: '20px' }}
-      >
-        Sign Up
-      </Button>
-      {/* <List>
-        {players.map((player, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={player.name} secondary={player.hasPaid ? 'Paid' : 'Not Paid'} />
-          </ListItem>
-        ))}
-      </List> */}
+          label="Your Name"
+          variant="outlined"
+          fullWidth
+          value={playerName}
+          onChange={handleNameChange}
+          margin="normal"
+        />
+        <Button
+          type="submit" 
+          variant="contained"
+          color="primary"
+          disabled={!playerName.trim()}
+          style={{ marginBottom: '20px' }}
+        >
+          Sign Up
+        </Button>
+      </form>
+      <PlayerList players={players} maxPlayers={gameDetails?.MaxPlayers} highlightedIndex={highlightedIndex} />
     </Container>
   );
 };
