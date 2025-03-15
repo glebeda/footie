31dc87cf7ae7016901e3c9ec0f794038ec589230
Georgii Lebeda <lebeda.georgiy@gmail.com>
2025-03-15 22:58:56 +0000

    Uncoment real implementation of cron

--- backend/src/api/routes/cron.js
+++ backend/src/api/routes/cron.js
@@ -14,23 +14,22 @@ const verifyCronRequest = (req, res, next) => {
 
 // Route to update game status - using GET since Vercel cron jobs use GET by default
 router.get('/update-game-status', verifyCronRequest, async (req, res) => {
-  console.log('Cron endpoint called for updating game status');
   
   try {
     console.log('Running cron job to update game status to PLAYED');
-    // const upcomingGame = await gameService.findUpcomingGame();
+    const upcomingGame = await gameService.findUpcomingGame();
     
-    // if (upcomingGame) {
-    //   await gameService.updateGameStatus(
-    //     upcomingGame.GameId,
-    //     GameStatus.PLAYED
-    //   );
-    //   console.log('Game status updated successfully');
+    if (upcomingGame) {
+      await gameService.updateGameStatus(
+        upcomingGame.GameId,
+        GameStatus.PLAYED
+      );
+      console.log('Game status updated successfully');
       return res.status(200).json({ success: true, message: 'Game status updated successfully' });
-    // } else {
-    //   console.log('No upcoming game found to update');
-    //   return res.status(404).json({ success: false, message: 'No upcoming game found to update' });
-    // }
+    } else {
+      console.log('No upcoming game found to update');
+      return res.status(404).json({ success: false, message: 'No upcoming game found to update' });
+    }
   } catch (error) {
     console.error('Failed to update game status:', error);
     return res.status(500).json({ success: false, error: error.message });
